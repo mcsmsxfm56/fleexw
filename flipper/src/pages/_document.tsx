980a30b7254ec484354e8711bbd065a5a5adce18
mcsmsxfm56 <mcuadrado135@gmail.com>
2023-04-17 10:57:10 -0300

    README actualizado y esqueleto de rutas basicas hecho

--- flipper/src/pages/_document.tsx
+++ flipper/src/pages/_document.tsx
@@ -1,5 +1,11 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+//_document edita todo los heads y body de todas las paginas
 
+//extracto de los docs de nextjs
+//The <Head /> component used in _document is not the same as next/head. The <Head />
+//component used here should only be used for any <head> code that is common for all
+//pages. For all other cases, such as <title> tags, we recommend using next/head in
+//your pages or components.
 export default function Document() {
   return (
     <Html lang="en">
@@ -9,5 +15,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
