a5fe6b50b7368c20b5cf6b0112a814ea2c5e339f
Franco Aglieri <francoaglieri@hotmail.com>
2023-04-19 16:17:13 -0300

    funcionalidad update trabajador terminada

--- flipper/src/pages/api/trabajador/[id].ts
+++ flipper/src/pages/api/trabajador/[id].ts
@@ -8,9 +8,9 @@ export default async function handler(
   res: NextApiResponse<DataTRegister | string>
 ) {
   const { id } = req.query;
-  console.log(id);
+  const body = req.body;
   const { authorization } = req.headers;
-  console.log(authorization);
+
   try {
     if (req.method === "GET") {
       const trabajador = await prisma.trabajador.findUnique({
@@ -21,16 +21,39 @@ export default async function handler(
         : res.status(400).send("no se pudo encontrar el trabajador");
     }
     if (req.method === "PUT") {
-      /// EN ESPERA DEL TOKEN --- NO TOCAR
-      if (!id) throw new Error("Token inexistente o invalido");
-      const token = authorization?.split(" ")[1];
-      //const decodedToken = jwt.verify(token, process.env.SECRET_KEY as string);
-      //console.log("2", decodedToken);
-      const user = await prisma.trabajador.findUnique({
-        where: { id: id as string },
-      });
-      console.log(user);
-      if (user) return res.status(200).send("user encontrado");
+      let token = null;
+      if (
+        authorization &&
+        authorization.toLocaleLowerCase().startsWith("bearer")
+      ) {
+        token = authorization.split(" ")[1]; // obtenemos el token del authorization 'bearer token'
+      }
+      if (!token) {
+        return res.status(401).send("token missing or invalid admin");
+      }
+      const decodedToken = jwt.verify(token, process.env.SECRET_KEY as string);
+      if (decodedToken) {
+        const trabajadorModificar = await prisma.trabajador.update({
+          where: { id: decodedToken.id },
+          data: {
+            phone: null ?? body.phone,
+            email: null ?? body.email,
+            nacimiento: null ?? body.nacimiento,
+            genero: null ?? body.genero,
+            ciudad: null ?? body.ciudad,
+            direccion: null ?? body.direccion,
+            estatura: null ?? body.estatura,
+            talla_camiseta: null ?? body.talla_camiseta,
+            grupo_sanguineo: null ?? body.grupo_sanguineo,
+            imagen_dni: null ?? body.imagen_dni,
+            foto: null ?? body.foto,
+            cv: null ?? body.cv,
+            rut: null ?? body.rut,
+            certificado_bancario: null ?? body.certificado_bancario,
+          },
+        });
+        return res.status(200).send(trabajadorModificar);
+      }
     }
   } catch (error: any) {
     res.status(400).send(error.message);
