065ae8fbf4382a5c513983f1cd66a88a067d1df1
SebasUNLu <SebasUNLu@gmail.com>
2023-04-20 10:22:57 -0300

    Arreglo detalle del formulario wordker

--- flipper/src/services/PostRegister.ts
+++ flipper/src/services/PostRegister.ts
@@ -4,7 +4,6 @@ import axios, { AxiosError } from "axios";
 const URL = 'http://localhost:3000'
 
 export async function Post_Company_Register(data: CompanyData) {
-  const sendData = { ...data, rol: 'empresa' }
   console.log("=== Company Data ===")
   console.log(data)
   console.log("=====")
@@ -12,10 +11,10 @@ export async function Post_Company_Register(data: CompanyData) {
 }
 
 export async function Post_Worker_Register(data: WorkerRegisterData) {
-  // TODO el numero de id llega como string, lo paso 
+  // TODO el numero de id llega como string, lo paso a number
   const sendData = { ...data, rol: 'trabajador', idNumber: parseInt(data.idNumber) }
   console.log("=== Worker Data ===")
   console.log(sendData)
   console.log("=====")
-  return axios.post(`${URL}/api/users/register/trabajador`, data)
+  return axios.post(`${URL}/api/users/register/trabajador`, sendData)
 }
