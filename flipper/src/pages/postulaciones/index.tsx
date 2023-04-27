e5d4167666e9ecf12bb627d821a7d4c7ada0ff2d
Pablo Swis <94465269+Pabloswis@users.noreply.github.com>
2023-04-27 18:47:34 -0300

    perfil trabajador responsive

--- flipper/src/pages/postulaciones/index.tsx
+++ flipper/src/pages/postulaciones/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
+const centroPantalla = 'flex justify-center items-center'
+
+
 const DetallePostulado = () => {
   return (
-    <div>DetallePostulado</div>
+    <div className='h-screen bg-slate-500 sm:bg-blue-400 md:bg-red-600 flex justify-center items-center'>
+      <h1 className=' text-black sm:text-red-700 sm:text-3xl'>Detalle del postulante</h1>
+
+    </div>
   )
 }
 
