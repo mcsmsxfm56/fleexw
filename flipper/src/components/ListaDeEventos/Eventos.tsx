67d34cb902097e377b97ab699be9d2d7b542fd11
mcsmsxfm56 <mcuadrado135@gmail.com>
2023-04-28 13:59:08 -0300

    Eventos cambiado a swt optimizado

--- flipper/src/components/ListaDeEventos/Eventos.tsx
+++ flipper/src/components/ListaDeEventos/Eventos.tsx
@@ -1,6 +1,6 @@
 import ListaEventos from "./ListaDeEventos";
 import { useSesionUsuarioContext } from "@/hooks/useSesionUsuarioContext";
-import axios from "axios";
+import useSWR, { Fetcher } from "swr";
 import React, { useState } from "react";
 
 export interface evento {
@@ -18,15 +18,38 @@ export interface Props {
 }
 
 type Ordering = "asc" | "desc";
+
 const buttonStyle =
   "btn bg-[#4B39EF] normal-case text-[24px] text-white border-transparent hover:bg-[#605BDC]";
 
+const fetcher: Fetcher<any, string> = (apiRoute) => {
+  return fetch(apiRoute, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      realmethod: "GET",
+      nombreEmpresa: localStorage.getItem("nombre"),
+    }),
+  }).then((res) => res.json());
+};
+
+//string define el tipado de la url recibida, any el tipado de la respuesta
 const Eventos: React.FC = () => {
-  const [eventos, setEventos] = useState<Props>({ eventos: [] });
-  const userContext = useSesionUsuarioContext();
+  let { isLoading, error, data } = useSWR("/api/empresa", fetcher);
+  //const [data2, setData] = useState({ eventos: [] });
+  //if (!isLoading) {
+  //setData(data);
+  //}
+  //console.log(data);
+  //console.log(error);
+  //console.log(isLoading);
+
+  //console.log(data);
+  //const userContext = useSesionUsuarioContext();
 
   const [order, setOrder] = useState<Ordering>("desc");
-
+  //const [data2, setData] = useState();
+  /*
   const userEvent = async () => {
     const sessionName = localStorage.getItem("nombre");
     await axios
@@ -41,10 +64,10 @@ const Eventos: React.FC = () => {
   React.useEffect(() => {
     userEvent();
   }, []);
-
+  */
   React.useEffect(() => {
     ordering(order);
-  }, [order]);
+  }, [order, data]);
 
   const ordering = (order: Ordering) => {
     function orderAsc(a: evento, b: evento) {
@@ -59,21 +82,29 @@ const Eventos: React.FC = () => {
       return 0;
     }
 
-    let sorted = [];
-    if (order == "asc") {
-      sorted = [...eventos.eventos].sort(orderAsc);
-    } else {
-      sorted = [...eventos.eventos].sort(orderDesc);
+    let sorted: any = [];
+    if (order == "asc" && isLoading === false) {
+      sorted = data.eventos.sort(orderAsc);
+    } else if (order == "desc" && isLoading === false) {
+      sorted = data.eventos.sort(orderDesc);
     }
 
     const eventosSorted = {
-      ...eventos,
+      ...data,
       eventos: sorted,
     };
-    setEventos(eventosSorted);
+    data = eventosSorted;
+    //console.log(data);
   };
 
   //console.log(eventos);//toda la info del user empresa, los eventos estan en eventos.eventos
+
+  if (error) {
+    console.log(error);
+    return <div>ERROR</div>;
+  }
+  //if (isLoading) return <div>Cargando...</div>;
+
   return (
     <div
       className="h-screen w-full bg-gray-200 md:ml-[10%] lg:ml-[250px]
@@ -100,7 +131,7 @@ const Eventos: React.FC = () => {
         </div>
       </div>
       <div className="p-2 lg:flex lg:justify-center">
-        <ListaEventos eventos={eventos?.eventos} />
+        <ListaEventos eventos={data?.eventos} />
       </div>
     </div>
   );
