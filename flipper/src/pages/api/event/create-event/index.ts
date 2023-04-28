a2a3564943e4bef2964851c5e3f6c8eac76f85df
mcsmsxfm56 <mcuadrado135@gmail.com>
2023-04-28 15:28:05 -0300

    backend createEventForm actualizado para funcionar en deploy

--- flipper/src/pages/api/event/create-event/index.ts
+++ flipper/src/pages/api/event/create-event/index.ts
@@ -134,6 +134,61 @@ export default async function handler(
     */
     }
   }
+  if (req.method === "PUT" && req.body.realmethod === "PUT") {
+    interface putEvento {
+      isDeleted?: boolean;
+      nombre?: string;
+      fecha?: string | Date;
+      hora_final?: string | Date;
+      lugar?: string;
+      cupos?: number;
+      perfil?: string;
+      pago?: number;
+      observaciones?: string;
+      trabajadores?: string;
+    }
+    const id: string = req.body.idEvent as string;
+    //console.log(id);
+    if (Object.keys(req.body.values).length === 0) {
+      res.status(400).send("Objeto vacio");
+    }
+    try {
+      if (!id) throw new Error("No existe ese evento");
+      let {
+        isDeleted,
+        trabajadores,
+        nombre,
+        fecha,
+        hora_final,
+        lugar,
+        cupos,
+        perfil,
+        pago,
+        observaciones,
+      }: putEvento = req.body.values;
+
+      const evento = await prisma.evento.update({
+        where: {
+          id: id,
+        },
+        data: {
+          nombre: nombre,
+          fecha_inicio: fecha,
+          fecha_final: hora_final,
+          lugar: lugar,
+          cupos: cupos,
+          perfil: perfil,
+          pago: pago,
+          observaciones: observaciones,
+        },
+      });
+      if (evento) {
+        return res.status(200).send("Evento actualizado con exito");
+      }
+    } catch (error: any) {
+      return res.status(400).send(error.message);
+    }
+  }
 }
 
 /*RUTA CREATE-EVENT/:ID
@@ -141,18 +196,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../../../lib/prisma";
 
 
-interface putEvento {
-  isDeleted?: boolean;
-  nombre?: string;
-  fecha?: string | Date;
-  hora_final?: string | Date;
-  lugar?: string;
-  cupos?: number;
-  perfil?: string;
-  pago?: number;
-  observaciones?: string;
-  trabajadores?: string;
-}
+
 
 export default async function handler(
   req: NextApiRequest,
@@ -185,50 +229,8 @@ export default async function handler(
     });
 
     res.status(200).send("deleted");
-  } else if (req.method === "PUT") {
-    const id: string = req.query.id as string;
-    //console.log(id);
-    if (Object.keys(req.body).length === 0) {
-      res.status(400).send("Objeto vacio");
-    }
-    try {
-      if (!id) throw new Error("No existe ese evento");
-      let {
-        isDeleted,
-        trabajadores,
-        nombre,
-        fecha,
-        hora_final,
-        lugar,
-        cupos,
-        perfil,
-        pago,
-        observaciones,
-      }: putEvento = req.body;
-
-      const evento = await prisma.evento.update({
-        where: {
-          id: id,
-        },
-        data: {
-          nombre: nombre,
-          fecha_inicio: fecha,
-          fecha_final: hora_final,
-          lugar: lugar,
-          cupos: cupos,
-          perfil: perfil,
-          pago: pago,
-          observaciones: observaciones,
-        },
-      });
-      if (evento) {
-        return res.status(200).send("Evento actualizado con exito");
-      }
-    } catch (error: any) {
-      return res.status(400).send(error.message);
-    }
-
-  }
+    
+  } else 
 }
 
 */
