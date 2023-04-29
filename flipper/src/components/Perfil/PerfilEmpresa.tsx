3360c9b627b3b124aa985c40b6d05c0f4a6edc80
Pablo Swis <94465269+Pabloswis@users.noreply.github.com>
2023-04-29 18:19:08 -0300

    modificar perfil

--- flipper/src/components/Perfil/PerfilEmpresa.tsx
+++ flipper/src/components/Perfil/PerfilEmpresa.tsx
@@ -50,7 +50,7 @@ const PerfilEmpresa: React.FC = () => {
 
 
   return (
-<<<<<<< HEAD
+
     <div className='flex justify-center items-center h-screen'>
 
       <div className=" bg-[#D9D9D9] border-2 rounded-3xl border-[#4B39EF] divide-solid p-5 md:p-5">
@@ -81,83 +81,12 @@ const PerfilEmpresa: React.FC = () => {
           <div className='flex justify-between m-4 md:mx-10'>
             <button onClick={() => alert('Esta funcion todavia no está disponible')} className={stylesProfile.button}>Modificar</button>
             <button onClick={() => alert('falta implementar')} className={stylesProfile.button}>Volver</button>
-=======
-    <div
-      className="h-screen w-full bg-gray-200 md:w-full md:ml-[4%] lg:ml-[250px]
-            lg:w-[calc(100vw-268px)] flex justify-center items-center "
-    >
-      <div className="card lg:w-6/12 h-4/5 lg:pt-0 bg-#1E1E1E shadow-inner border-t-2 border-b-2 rounded-md border-[#4B39EF]">
-        <div className="card-body p-2 flex flex-col justify-center h-full w-full">
-          <figure className="mb-6">
-            <Image
-              className="w-36 text-black"
-              src={perfilProvisorio}
-              alt="Picture of the author"
-            />
-          </figure>
-          <div className="mb-6">
-            <h2 className="text-3xl font-bold text-indigo-600 capitalize text-center">
-              {profile?.nombre}
-            </h2>
-          </div>
-          <div className="ml-2 md:flex md:flex-col md:items-start lg:ml-16 2xl:ml-56">
-            <h5 className="text-lg md:text-2xl md:text-left p-2 font-bold text-black">
-              Nombre Ceo: {profile?.nombreceo}
-            </h5>
-            <h5 className="text-lg md:text-2xl md:text-left font-bold p-2 text-black">
-              Ciudad: {profile?.ciudad}
-            </h5>
-            <h5 className="text-lg md:text-2xl md:text-left font-bold p-2 text-black">
-              Dirección: {profile?.direccion}
-            </h5>
-            <h5 className="text-lg md:text-2xl md:text-left font-bold p-2 text-black">
-              Telfono: {profile?.telefono}
-            </h5>
-            <h5 className="text-lg md:text-2xl md:text-left font-bold p-2 text-black">
-              Email: {profile?.email}
-            </h5>
->>>>>>> 2f9890d9943db6cb39821b7a5455f9a8f48f07fd
+
           </div>
         </div>
       </div>
     </div >
-    // <div
-    //   className="h-screen w-full bg-gray-200 md:w-full md:ml-[4%] lg:ml-[250px]
-    //         lg:w-[calc(100vw-268px)] flex justify-center items-center ">
-    //   <div className="card lg:w-6/12 h-4/5 lg:pt-0 bg-#1E1E1E shadow-inner border-t-2 border-b-2 rounded-md border-[#4B39EF]">
-    //     <div className="card-body p-2 flex flex-col justify-center h-full w-full">
-    //       <figure className="mb-6">
-    //         <Image
-    //           className="w-36 text-black"
-    //           src={perfilProvisorio}
-    //           alt="Picture of the author"
-    //         />
-    //       </figure>
-    //       <div className="mb-6">
-    //         <h2 className="text-3xl font-bold text-indigo-600 capitalize text-center">
-    //           {profile?.nombre}
-    //         </h2>
-    //       </div>
-    //       <div className="ml-2 md:flex md:flex-col md:items-start lg:ml-16 2xl:ml-56">
-    //         <h5 className="text-lg md:text-2xl md:text-left p-2 font-bold text-black">
-    //           Nombre Ceo: {profile?.nombreceo}
-    //         </h5>
-    //         <h5 className="text-lg md:text-2xl md:text-left font-bold p-2 text-black">
-    //           Ciudad: {profile?.ciudad}
-    //         </h5>
-    //         <h5 className="text-lg md:text-2xl md:text-left font-bold p-2 text-black">
-    //           Dirección: {profile?.direccion}
-    //         </h5>
-    //         <h5 className="text-lg md:text-2xl md:text-left font-bold p-2 text-black">
-    //           Telfono: {profile?.telefono}
-    //         </h5>
-    //         <h5 className="text-lg md:text-2xl md:text-left font-bold p-2 text-black">
-    //           Email: {profile?.email}
-    //         </h5>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
+
   );
 };
 
