c16a6ac463c1959e5951df8217ac0099135659f1
SebasUNLu <SebasUNLu@gmail.com>
2023-05-04 21:35:30 -0300

    style: card de historial del trabajador

--- flipper/src/components/ListaDeEventos/ListaEventosTrabajador.tsx
+++ flipper/src/components/ListaDeEventos/ListaEventosTrabajador.tsx
@@ -4,19 +4,20 @@ import { EventCard } from "./EventCard";
 import { DetalleEvento } from "@/types/Types";
 
 interface eventoTrabajador {
-  evento: DetalleEvento
-  eventoId: string
-  notificacionVista: boolean
-  status: string
-  trabajadorId: string
+  evento: DetalleEvento;
+  eventoId: string;
+  notificacionVista: boolean;
+  status: string;
+  trabajadorId: string;
 }
+
+const aprobadoStyle = "text-green-700 border-green-700 bg-green-300";
+const faltoStyle = "text-red-700 border-red-700 bg-red-300";
 
 interface Props {
   eventos: [] | undefined;
 }
 const ListaEventosTrabajador: React.FC<Props> = ({ eventos }) => {
-
-
   if (!eventos) {
     return (
       <div>
@@ -29,17 +30,45 @@ const ListaEventosTrabajador: React.FC<Props> = ({ eventos }) => {
     <div className="w-full md:w-10/12 lg:w-3/5">
       {eventos ? (
         eventos.map((event: eventoTrabajador) => {
-
-          if (event.status === 'APROBADO') {
+          if (event.status === "APROBADO") {
             return (
-              <div className="bg-white rounded-md border-2 border-[#787d81] h-full flex flex-col justify-between p-2 mb-2 w-full">
-                <div key={`${event.eventoId}_key`}>
-                  <h2>Status: {event.status}</h2>
-                  <h2>Evento: {event.evento.nombre}</h2>
-                  <h2>Lugar: {event.evento.lugar}</h2>
-                  <h2>Fecha: {event.evento.fecha_final.slice(0, 10)}</h2>
-                  <h2>Pago: ${event.evento.pago}</h2>
-                  <h2>Observaciones: {event.evento.observaciones}</h2>
+              <div className="bg-white rounded-md border-2 border-[#787d81] h-full flex flex-col justify-between p-2 mb-2 w-full text-indigo-700">
+                <div key={`${event.eventoId}_key`} className="flex flex-col">
+                  <h2
+                    className={`flex justify-start font-bold self-center border-2 border-solid px-8 py-1 rounded-xl
+                  ${
+                    event.status === ("APROBADO" || "ASISTIO")
+                      ? aprobadoStyle
+                      : faltoStyle
+                  }
+                  `}
+                  >
+                    {event.status}
+                  </h2>
+                  <h2 className="flex justify-start font-bold">
+                    Evento:{" "}
+                    <p className="font-normal ml-2">{event.evento.nombre}</p>
+                  </h2>
+                  <h2 className="flex justify-start font-bold">
+                    Lugar:{" "}
+                    <p className="font-normal ml-2">{event.evento.lugar}</p>
+                  </h2>
+                  <h2 className="flex justify-start font-bold">
+                    Fecha:{" "}
+                    <p className="font-normal ml-2">
+                      {event.evento.fecha_final.slice(0, 10)}
+                    </p>
+                  </h2>
+                  <h2 className="flex justify-start font-bold">
+                    Pago:{" "}
+                    <p className="font-normal ml-2">${event.evento.pago}</p>
+                  </h2>
+                  <h2 className="flex justify-start font-bold">
+                    Observaciones:{" "}
+                  </h2>
+                  <p className="font-normal ml-2">
+                    {event.evento.observaciones}
+                  </p>
                 </div>
               </div>
             );
