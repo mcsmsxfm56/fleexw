19b2641a04ff07aac6a156b4db693359c19d962b
mcsmsxfm56 <mcuadrado135@gmail.com>
2023-05-07 00:44:13 -0300

    excel dashboard terminado

--- flipper/src/components/Excel/generateExcel.ts
+++ flipper/src/components/Excel/generateExcel.ts
@@ -1,9 +1,21 @@
+import { objEmpresa, objEvento, objTrabajador } from "@/types/Types";
 import * as XLSX from "xlsx";
 
-const downloadExcel = (data: any) => {
-  const worksheet = XLSX?.utils.json_to_sheet(data);
-  const workbook = XLSX?.utils.book_new();
-  XLSX?.utils.book_append_sheet(workbook, worksheet, "Sheet1");
-  XLSX?.writeFile(workbook, "DataSheet.xlsx");
+export const downloadExcelAdmin = (
+  sheetTrabajador: objTrabajador[],
+  sheetEmpresa: objEmpresa[],
+  sheetEventos: objEvento[]
+) => {
+  const sheetTrabajador2 = XLSX?.utils.json_to_sheet(sheetTrabajador);
+  const sheetEmpresa2 = XLSX?.utils.json_to_sheet(sheetEmpresa);
+  const sheetEventos2 = XLSX?.utils.json_to_sheet(sheetEventos);
+  const adminData = XLSX?.utils.book_new();
+  XLSX?.utils.book_append_sheet(
+    adminData,
+    sheetTrabajador2,
+    "Tabla Trabajadores"
+  );
+  XLSX?.utils.book_append_sheet(adminData, sheetEmpresa2, "Tabla Empresa");
+  XLSX?.utils.book_append_sheet(adminData, sheetEventos2, "Tabla Eventos");
+  XLSX?.writeFile(adminData, "adminData.xlsx");
 };
-export default downloadExcel;
--- flipper/src/pages/dashboard/index.tsx
+++ flipper/src/pages/dashboard/index.tsx
@@ -1,35 +1,26 @@
 import DatagridEmpresa from "./DatagridEmpresa";
 import DatagridEventos from "./DatagridEventos";
 import DatagridTrabajadores from "./DatagridTrabajadores";
-import downloadExcel from "../../components/Excel/generateExcel";
+import { downloadExcelAdmin } from "../../components/Excel/generateExcel";
 import useSWR from "swr";
 import { fetcherDashboard } from "./middlewareDashboard";
+import { objEmpresa, objEvento, objTrabajador } from "@/types/Types";
 
 export default function Dashboard() {
   const { isLoading, error, data } = useSWR("/api/admin", fetcherDashboard);
   console.log(data); //{empresaTable: [{objEmpresa}], eventoTable: [{objEvento}], trabajadorTable: [{objTrabajador}]}
-  const sheetTrabajador = [
-    {
-      column1: 1,
-      temaIndicador: "Indian",
-      codigo: "001",
-      observaciones: "Interactions Specialist tertiary Regional Tennessee",
-      activo: "SI",
-      urlImagen: "http://placeimg.com/640/480",
-      color: "cyan",
-      createdAt: "2022-01-26T18:48:36.002Z",
-    },
-    {
-      column1: 2,
-      temaIndicador: "Indian",
-      codigo: "001",
-      observaciones: "Interactions Specialist tertiary Regional Tennessee",
-      activo: "SI",
-      urlImagen: "http://placeimg.com/640/480",
-      color: "cyan",
-      createdAt: "2022-01-26T18:48:36.002Z",
-    },
-  ];
+  const sheetTrabajador: objTrabajador[] = [];
+  const sheetEmpresa: objEmpresa[] = [];
+  const sheetEventos: objEvento[] = [];
+  data?.empresaTable.map((objEmpresa: objEmpresa) => {
+    sheetEmpresa.push(objEmpresa);
+  });
+  data?.eventoTable.map((objEvento: objEvento) => {
+    sheetEventos.push(objEvento);
+  });
+  data?.trabajadorTable.map((objTrabajador: objTrabajador) => {
+    sheetTrabajador.push(objTrabajador);
+  });
   if (isLoading) return <div>Cargando...</div>;
   return (
     <div>
@@ -41,7 +32,7 @@ export default function Dashboard() {
       <DatagridTrabajadores dataTrabajadores={data?.trabajadorTable} />
       <button
         onClick={() => {
-          downloadExcel(sheetTrabajador);
+          downloadExcelAdmin(sheetTrabajador, sheetEmpresa, sheetEventos);
         }}
       >
         Descargar Excel
