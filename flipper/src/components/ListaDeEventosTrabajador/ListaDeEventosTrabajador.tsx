b79310c466abd6d4ba1ef5cbf898c588f4b5bb69
SebasUNLu <SebasUNLu@gmail.com>
2023-05-12 17:47:39 -0300

    Fix: ahora no muestra pagos y cupos mezclados en la card disponible

--- flipper/src/components/ListaDeEventosTrabajador/ListaDeEventosTrabajador.tsx
+++ flipper/src/components/ListaDeEventosTrabajador/ListaDeEventosTrabajador.tsx
@@ -51,8 +51,8 @@ const ListaEventosTrabajador: React.FC<Props2> = ({ eventos }) => {
                   lugar={event.lugar}
                   isDeleted={event.isDeleted}
                   id={event.id}
-                  pago={event.cupos}
-                  cupos={event.pago}
+                  pago={event.pago}
+                  cupos={event.cupos}
                   admitePostulaciones={event.admitePostulaciones}
                   establecimiento={event.establecimiento}
                   numeroPostulantes={event.numeroPostulantes}
