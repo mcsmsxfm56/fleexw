03bec38b2181d8deb1c755e6782be90e7090f237
mcsmsxfm56 <mcuadrado135@gmail.com>
2023-05-11 22:45:19 -0300

    arreglado botn datagrid

--- flipper/src/pages/evento/detalle/[idEvent].tsx
+++ flipper/src/pages/evento/detalle/[idEvent].tsx
@@ -80,7 +80,12 @@ const EventDatail = () => {
           onClick={() => {
             let idPostulante = params.row.UUID;
             let statusNuevo = params.row.Status;
-            aceptarORechazarPostulante({ idPostulante, statusNuevo, idEvent });
+            aceptarORechazarPostulante({
+              idPostulante,
+              statusNuevo,
+              idEvent,
+              token,
+            });
           }}
         >
           Actualizar
@@ -138,9 +143,7 @@ const EventDatail = () => {
   return (
     <AppLayout>
       <div className="h-full">
-        <div
-          className="bg-gray-200"
-        >
+        <div className="bg-gray-200">
           <div className="flex flex-col justify-center items-center gap-10 relative">
             <div className="w-full flex flex-row justify-between items-center mt-16 md:mt-0">
               <p className="w-full bg-white text-center text-[#4B39EF] font-bold text-xl py-4 relative">
@@ -191,7 +194,9 @@ const EventDatail = () => {
               </div>
             </div>
             <div className="flex flex-col">
-              <p className="text-center font-bold text-xl text-black">Observaciones:</p>
+              <p className="text-center font-bold text-xl text-black">
+                Observaciones:
+              </p>
               <p className="text-center font-bold text-lg">
                 {eventDetail?.observaciones}
               </p>
--- flipper/src/services/aceptarORechazarPostulante.ts
+++ flipper/src/services/aceptarORechazarPostulante.ts
@@ -4,17 +4,20 @@ interface aceptarORechazarPostulante {
   idPostulante: string;
   statusNuevo: string;
   idEvent: string | string[] | undefined;
+  token: string;
 }
 
 export const aceptarORechazarPostulante = async ({
   idPostulante,
   statusNuevo,
   idEvent,
+  token,
 }: aceptarORechazarPostulante) => {
   const response = await fetch(`/api/event`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
     },
     body: JSON.stringify({
       eventoId: idEvent,
